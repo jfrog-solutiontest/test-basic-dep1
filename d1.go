3867518fe08c22ce11bfed66b28b169361f328b8
jfrog-solutiontest <jfrog-solutionstest-ci@jfrog.com>
2020-07-22 16:41:35 -0700

    initial release

---
+++ d1.go
@@ -0,0 +1,24 @@
+package ci_basic_dep1
+
+import (
+	"fmt"
+	"github.com/jfrog-solutiontest/ci-basic_dep2"
+	"github.com/miekg/dns"
+)
+
+func PackageName () {
+	fmt.Println ("Package Name: d1")
+	ci_basic_dep2.PackageName()
+}
+
+func updateDomainWithCName(r *dns.Msg, fqdn string) string {
+	for _, rr := range r.Answer {
+		if cn, ok := rr.(*dns.CNAME); ok {
+			if cn.Hdr.Name == fqdn {
+				return cn.Target
+			}
+		}
+	}
+
+	return fqdn
+}
