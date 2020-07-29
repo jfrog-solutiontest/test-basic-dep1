be2f722ecaab02cbc64d454b043c87a5131a17ba
jfrog-solutiontest <jfrog-solutions-ci@jfrog.com>
2020-07-29 08:25:34 -0700

    initial

--- d1.go
+++ d1.go
@@ -2,13 +2,13 @@ package d1
 
 import (
 	"fmt"
-	"github.com/jfrog-solutiontest/ci-basic-dep2"
+	"github.com/jfrog-solutiontest/test-basic-dep2"
 	"github.com/miekg/dns"
 )
 
 func PackageName () {
 	fmt.Println ("Package Name: d1");
-	ci-basic-dep2.PackageName()
+	test-basic-dep2.PackageName()
 }
 
 func updateDomainWithCName(r *dns.Msg, fqdn string) string {
